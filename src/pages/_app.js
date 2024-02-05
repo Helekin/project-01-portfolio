3baf75f06be66eab121410305651ae51e0ac515b
MasterHellekin <clsjs94@hotmail.com>
2024-02-05 17:21:07 -0500

    features

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,12 @@
+import { useEffect } from "react";
+
+import "@/styles/portfolio.scss";
 import "@/styles/globals.css";
 
 export default function App({ Component, pageProps }) {
+  useEffect(() => {
+    import("bootstrap/dist/js/bootstrap");
+  }, []);
+
   return <Component {...pageProps} />;
 }
