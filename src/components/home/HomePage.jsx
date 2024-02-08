8ef3b5b1ff047c0c850f99bb38555a844274fb87
MasterHellekin <clsjs94@hotmail.com>
2024-02-08 12:27:49 -0500

    background image

--- src/components/home/HomePage.jsx
+++ src/components/home/HomePage.jsx
@@ -1,23 +1,24 @@
 import { Lora } from "next/font/google";
+import Image from "next/image";
 
 const lora = Lora({ subsets: ["latin"] });
 
 const HomePage = () => {
   return (
-    <header className="header vh-100 text-center position-relative">
-      <div className="text-container position-relative d-flex flex-column justify-content-center align-items-center h-100">
+    <div className="header-container">
+      <Image src="/intro-bg.png" alt="intro bg" fill={true} />
+      <div className="text-container">
         <h5
           className={`${lora.className} text-primary fs-3 fw-bold text-uppercase`}
         >
           Hello, World
         </h5>
-        <h1 className="display-1 fw-bold text-white"></h1>
         <p className="roles text-white text-uppercase fs-4">
           <span>Backend Developer | </span>
           <span>AWS Expert</span>
         </p>
       </div>
-    </header>
+    </div>
   );
 };
 
